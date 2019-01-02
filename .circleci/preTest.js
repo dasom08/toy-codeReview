5b7304cafe4ef97d6115dbfc8c97faa01cfad8f8
Sangho <sangho@Sanghos-MacBook-Air.local>
2019-01-03 00:00:39 +0900

    Implement submit.js
    After test finihsed, get the test result from circleCI console,
    get the student's data form the student.json, and send them to the spread sheet.
    
    Add some code in preTest.js that throw error if students did not put their data in the json.

--- .circleci/preTest.js
+++ .circleci/preTest.js
@@ -2,4 +2,8 @@ let studentInfo = require('../student.json')
 
 let {th, name, problemNumber} = studentInfo
 
+if (th === "" | name === "") {
+    throw new Error("Studnets Information is empty! put your info in student.json")
+}
+
 console.log(problemNumber);
---
+++ .circleci/submit.js
@@ -0,0 +1,59 @@
+const { exec } = require('child_process');
+const https = require('https');
+const moment = require('moment-timezone')
+
+// Get sutdent data from student.json
+let studentInfo = require('../student.json')
+let {th, name, problemNumber} = studentInfo
+
+// Create date time when the problem is submited
+moment.tz.setDefault("Asia/Seoul")
+let date = moment().format('YYYY-MM-DD')
+
+exec('npm test | grep -E \"[0-9]+\\s(passing|failing)\"', (err, stdout1, stderr) => {
+    if (err) {
+        return;
+    }
+
+    // Get test result from the console and cleasing it for spread sheet
+    let matchWithPassing = stdout1.match(/([.\d,]+)[ ]+passing/)
+    let matchWithFaling = stdout1.match(/([.\d,]+)[ ]+failing/)
+    let passing = matchWithPassing ? Number(matchWithPassing[1]) : 0
+    let faling = matchWithFaling ? Number(matchWithFaling[1]) : 0
+
+    exec('echo "$airtable_api_key"', (err, apikey) => {
+        const options = {
+            hostname: 'api.airtable.com',
+            path: '/v0/app8kEq9wXlsuffDy/Toy%20Problem',
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            Authorization: ' Bearer keyXjjgq2u2fJH9Np'
+            }
+        };
+
+        const req = https.request(options, (res) => {
+            res.on('data', (chunk) => {
+            console.log(chunk.toString());
+          // callback(null, result);
+        });
+      });
+
+      req.on('error', (e) => {
+        console.log('error');
+        // callback(new Error('failure'));
+      });
+      // send the request
+      req.write(JSON.stringify({
+        'fields': {
+            'th': th,
+            'name':name,
+            'problem': problemNumber,
+            'passing': passing,
+            'failing': faling,
+            'date': date
+        }
+      }));
+      req.end();
+    });
+});
