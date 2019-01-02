74ea20d466242d983cfab12d5e2ca4c83a00c1f1
Sangho <sangho@Sanghos-MacBook-Air.local>
2019-01-03 01:27:48 +0900

    Change template string on the api header.
    change back tick template string to es5 string, to fix api request error.

--- .circleci/preTest.js
+++ .circleci/preTest.js
@@ -2,7 +2,7 @@ let studentInfo = require('../student.json')
 
 let {th, name, problemNumber} = studentInfo
 
-if (th === "" | name === "") {
+if (th === "" | name === "" | th === "Immersive 기수를 입력해 주세요" | name === "Put Your name") {
     throw new Error("Studnets Information is empty! put your info in student.json")
 }
 
