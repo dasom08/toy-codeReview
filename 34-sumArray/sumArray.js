c5be108691c551733ed61cb764950c3e818c24dc
gotoweb <igotoweb@gmail.com>
2020-04-05 01:57:26 +0900

    update toys 30~49

---
+++ 34-sumArray/sumArray.js
@@ -0,0 +1,13 @@
+/*
+ * Given an array of numbers, calculate the greatest contiguous(not be off) sum of elements in array.
+ * A single array item will count as a contiguous sum.
+ *
+ * example 1: sumArray([1, 2, 3]); // => 6
+ * example 2: sumArray([1, 2, 3, -4]); // sum of [1,2,3] === 6
+ * example 3: sumArray([1, 2, 3, -4, 5]); // sum of [3,-4, 5] === 7
+ * example 4: sumArray([4, -1, 5]); // => 8 sum of [4, -1, 5] === 8
+ * example 5: sumArray([10, -11, 11]); // [11] A siggle array item
+ */
+
+// Solved in O(n) time with O(1) memory
+var sumArray = function(array) {};
