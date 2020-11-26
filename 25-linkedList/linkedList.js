163e5ef797dd7a5aa240a7a57749a258219e1060
dasom <dasom.choi08@gmail.com>
2020-11-26 21:28:05 +0900

    toy-25 linkedList

--- 25-linkedList/linkedList.js
+++ 25-linkedList/linkedList.js
@@ -17,23 +17,54 @@
 // list.tail.value;   //yields '5';
 
 
-var LinkedList = function(){
+var LinkedList = function(value){
   //fill me in!
+  this.head = null
+  this.next = null 
+  this.tail = null
+  this.value = value;
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-){
+LinkedList.prototype.addToTail = function(value){
+  let node = new LinkedList(value)
+  if(this.tail === null){
+    node.next = this.head
+    this.head = node
+    this.tail = this.head
+  }
+  else if (this.tail){
+    this.tail.next = node; 
+    this.tail = node; 
+  }
 };
 
 LinkedList.prototype.removeHead = function(){
+  if(this.head.next){
+    this.head = this.head.next; 
+  }else{
+    this.head = null;
+    this.tail = null; 
+  }
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function(value){
+  if(!this.head){
+    return undefined;
+  }else{
+    let temp = this.head; 
+    while(temp.value !== value){
+      //4 , 4
+      if(temp.next === null){
+        return false 
+      }
+      temp = temp.next;
+    }
+  }
+  return true
 };
 
-LinkedList.prototype.makeNode = function(
-){
+LinkedList.prototype.makeNode = function(){
+
 };
